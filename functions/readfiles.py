5015d2959f2495e6b6050ee1d5dd77781b468ab4
OSAMUYI AIYEKI <muyipeters@gmail.com>
2025-07-23 15:55:18 +0100

    git commit -m "Update functions and files"

--- functions/readfiles.py
+++ functions/readfiles.py
@@ -4,6 +4,6 @@ print(f1.read())
 f1.close()
 
 print( "\nNow lets create another diary!")
-f2 = open("functions/files/diary.txt", "a")
+f2 = open("functions/files/diary.txt", "w")
 f2.write("Writing in my diary file.\n")
 f2.close()
